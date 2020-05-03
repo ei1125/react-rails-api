3187080fd6ecd89e1073f9c0235c3232d0a305bf
ei1125 <i.dont.like.monday1125@gmail.com>
2020-05-03 11:06:05 +0900

    implement event new

--- front_react_rails_api/src/actions/index.js
+++ front_react_rails_api/src/actions/index.js
@@ -1,10 +1,16 @@
 import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENT'
 
 const ROOT_URL = 'http://localhost:3001'
 
 export const readEvents = () => async dispatch => {
   const response = await axios.get(`${ROOT_URL}/todos`)
   dispatch({ type: READ_EVENTS, response })
+}
+
+export const postEvent = values => async dispatch => {
+  const response = await axios.post(`${ROOT_URL}/todos`, values)
+  dispatch({ type: CREATE_EVENT, response })
 }
--- front_react_rails_api/src/index.js
+++ front_react_rails_api/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new.js';
 import * as serviceWorker from './serviceWorker';
 
 
@@ -14,7 +16,13 @@ const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/events/new" component={EventsNew} />
+        <Route exact path="/" component={EventsIndex} />
+        <Route exact path="/events" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
